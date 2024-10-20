d20100896bd2d33b3484c4428379e23b3cc4e123
Edson Correia <edsoncorreia997@gmail.com>
2024-10-19 23:42:06 -0300

    criação e desenvolvimento segunda seção

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -35,6 +35,67 @@ export default function Home() {
 
             <section className='segunda-secao'>
                 <div className='segunda-secao-container'>
+                    <div>
+                        <h1 className='titulo'>
+                         Minha Formação
+                        </h1>
+                        <p className='informacao'>
+                        Um pouco sobre minha jornada acadêmica
+                        </p>
+                    </div>
+
+                    <div className='formacoes'>
+                        <div className='formacoes-item'>
+                            <img src='/assets/images/senaco.svg' alt='senac'/>
+
+                            <p className='formacoes-item-texto'>
+                            Bacharelado em Design
+                            <br/>
+                            <b>Senac São Paulo</b>
+                            </p>
+
+                            <img src='/assets/images/circulo.svg'/>
+
+                            <p className='formacoes-item-texto'>
+                                <b>2016</b>
+                            </p>
+
+                        </div>
+
+                        <div className='formacoes-item'>
+                            <img src='/assets/images/unesparo.svg' alt='senac'/>
+
+                            <p className='formacoes-item-texto'>
+                            Pós Graduação em Artes Visuais
+                            <br/>
+                            <b>Universidade Estadual do Paraná</b>
+                            </p>
+
+                            <img src='/assets/images/circulo.svg'/>
+
+                            <p className='formacoes-item-texto'>
+                                <b>2020</b>
+                            </p>
+
+                        </div>
+
+                        <div className='formacoes-item'>
+                            <img src='/assets/images/unespo.svg' alt='senac'/>
+
+                            <p className='formacoes-item-texto'>
+                            Doutorado em Artes
+                            <br/>
+                            <b>Universidade Estadual Paulista</b>
+                            </p>
+
+                            <img src='/assets/images/circulo.svg'/>
+
+                            <p className='formacoes-item-texto'>
+                                <b>2024</b>
+                            </p>
+
+                        </div>
+                    </div>
 
                 </div>
             </section>
