5588404dcbb4fd25b0df6d3ab8d27d72e7457c00
Edson Correia <edsoncorreia997@gmail.com>
2024-10-19 22:12:09 -0300

    criação e desenvolvimento primeira seção

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,12 +16,13 @@ export default function Home() {
                     Olá! Eu sou Heather, uma designer apaixonada por criar experiências visuais que conectam pessoas e ideias. Ao longo dos últimos 6 anos, tenho trabalhado com uma variedade de projetos, desde identidade visual e design editorial até interfaces digitais. Meu objetivo é sempre encontrar o equilíbrio entre funcionalidade e estética, criando soluções criativas que comunicam histórias de forma clara e impactante.
                     </p>
 
-                    <div>
-                        <button>
+                    <div className='primeira-secao-botoes'>
+                        <button className='botao'>
                             Fale Comigo
+                            <img src='/assets/images/Forward2.png' alt='seta'/>
                         </button>
 
-                        <button>
+                        <button className='botao secundario'>
                             Meus Serviços
                         </button>
                     </div>
@@ -29,6 +30,12 @@ export default function Home() {
                     
                 <div>
                 <img src='/assets/images/sobre-mim-fotos.png' className='sobre-mim-fotos' alt='Heather'/>
+                </div>
+            </section>
+
+            <section className='segunda-secao'>
+                <div className='segunda-secao-container'>
+
                 </div>
             </section>
         </div>
